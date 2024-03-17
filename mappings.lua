e73c84177b68199efe5b1bb0ea9cb9d443b2a981
RoccoRakete <m.schaumann@mscloud.uk>
2024-03-17 22:47:36 +0100

    nvim update

--- mappings.lua
+++ mappings.lua
@@ -17,7 +17,9 @@ return {
     --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
     --   desc = "Previous buffer",
     -- },
-
+    ["."] = {
+      ":"
+    },
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
       function()
---
+++ plugins/stylua-nvim.lua
@@ -0,0 +1,5 @@
+return {
+  {
+  "shoukoo/stylua.nvim"
+    },
+}
